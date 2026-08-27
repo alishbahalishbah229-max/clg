a8d3ab460fcc67b7a7ef8f35ed13a3d738076bab
alishbahalishbah229-max <alishbahalishbah229@gmail.com>
2026-08-27 13:07:25 +0500

    Update o-dashboard.php

--- o-dashboard.php
+++ o-dashboard.php
@@ -3554,7 +3554,7 @@ function getEventStatusLabel(
 
     </main>
 
-<?php require_once __DIR__ . '/../../includes/footer.php'; ?>
+<?php require_once __DIR__ . '/footer.php'; ?>
 </body>
 
 </html>
