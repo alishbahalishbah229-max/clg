27dcf44ea4fad2bd1b336effc1b02e2eb13eb5ea
alishbahalishbah229-max <alishbahalishbah229@gmail.com>
2026-08-27 12:12:02 +0500

    Fix require paths in registrations.php
    Updated file paths for required files in registrations.php.

--- registrations.php
+++ registrations.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../../config/database.php';
-require_once __DIR__ . '/../../includes/functions.php';
-require_once __DIR__ . '/../../includes/auth.php';
+require_once __DIR__ . '/database.php';
+require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/auth.php';
 
 requireRole('organizer');
 
