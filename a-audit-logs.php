d8428bc702273c8d33ded3bd66abe663f61361f7
alishbahalishbah229-max <alishbahalishbah229@gmail.com>
2026-08-27 15:45:12 +0500

    Update a-audit-logs.php

--- a-audit-logs.php
+++ a-audit-logs.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once __DIR__ . '/../../config/database.php';
-require_once __DIR__ . '/../../includes/functions.php';
-require_once __DIR__ . '/../../includes/auth.php';
+require_once __DIR__ . '/database.php';
+require_once __DIR__ . '/functions.php';
+require_once __DIR__ . '/auth.php';
 
 requireRole('admin');
 
@@ -2413,7 +2413,7 @@ h1{
 
 </main>
 
-<?php require_once __DIR__ . '/../../includes/footer.php'; ?>
+<?php require_once __DIR__ . '/footer.php'; ?>
 </body>
 
 </html>
