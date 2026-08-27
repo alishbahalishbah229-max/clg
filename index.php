099e00ed5ec5a88f3957227d03a5bd278370adb2
alishbahalishbah229-max <alishbahalishbah229@gmail.com>
2026-08-27 11:37:20 +0500

    Update file paths for required PHP files

--- index.php
+++ index.php
@@ -5,9 +5,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // 2. Sahi relative paths (Kyunki files direct root folder mein hain)
-require_once __DIR__ . '/database.php';
-require_once __DIR__ . '/functions.php';
-require_once __DIR__ . '/auth.php';
+require_once __DIR__ . '/config/database.php';
+require_once __DIR__ . '/includes/functions.php';
+require_once __DIR__ . '/includes/auth.php';
+
 
 // 3. CAMPUS360 MAIN HOMEPAGE REDIRECTION
 // Agar aapke paas 'login.php' ya koi aur main file hai toh user direct wahan chala jaye
