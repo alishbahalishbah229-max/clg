def913e93f5a0e8cd88127f5e01337899411464c
alishbahalishbah229-max <alishbahalishbah229@gmail.com>
2026-08-27 15:51:35 +0500

    Fix require path for auth.php

--- a-users.php
+++ a-users.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/database.php';
 require_once __DIR__ . '/functions.php';
-require_once __DIR__ . '//auth.php';
+require_once __DIR__ . '/auth.php';
 
 requireRole('admin');
 
